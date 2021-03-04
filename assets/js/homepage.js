b58f26caf1ab19f34baff80be6a989c842d3f526
sshahram <shirin.shahram23@gmail.com>
2021-03-04 14:51:11 -0800

    handle form submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,22 @@
+// global variables
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// form submission
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value= "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
 var getUserRepos = function (user) {
     // format the github url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -11,4 +30,4 @@ var getUserRepos = function (user) {
     });
 };
 
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
