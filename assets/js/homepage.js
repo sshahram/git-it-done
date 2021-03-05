0618fdad4e8a2f83f2b557770fd1b1aa58a57f83
sshahram <shirin.shahram23@gmail.com>
2021-03-05 00:44:37 -0800

    handling error 404

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -34,7 +34,7 @@ var getUserRepos = function (user) {
                 displayRepos(data, user);
             });
         } else {
-            alert("Error: " + response.statusText);
+            alert("Error: " + response.status);
         }
     })
     .catch(function(error) {
