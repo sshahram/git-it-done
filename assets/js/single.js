fda3d31f7c84b7ee562dca3d6ccbfbb56c280801
sshahram <shirin.shahram23@gmail.com>
2021-03-07 15:05:55 -0800

    add warning message for pagination

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl ="https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +9,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to DOM function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")){
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!")
@@ -51,9 +57,20 @@ var displayIssues = function(issues) {
 
         // append everythign to the page
         issueContainerEl.appendChild(issueEl);
-
     }
-
 };
 
-getRepoIssues("barakm/github-open-pull-requests");
+// display a warning if repo has more than 30 issues
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+getRepoIssues("facebook/react");
