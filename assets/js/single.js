19cba2a198aff7e144a7e0997fc128a0dc7e5724
sshahram <shirin.shahram23@gmail.com>
2021-03-07 17:51:24 -0800

    use query parameter

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,13 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl ="https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -73,4 +81,5 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 }
-getRepoIssues("facebook/react");
+
+getRepoName();
