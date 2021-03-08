b3da8a0698878b0603b50a55cd459452c30ebf6a
sshahram <shirin.shahram23@gmail.com>
2021-03-07 20:41:27 -0800

    search repositories by languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,6 +6,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
+
 // form submission
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -94,4 +96,31 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " +response.status)
+        }
+    });
+};
+
+// function for buttons for selecting languages
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if(language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }   
+};
+
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
