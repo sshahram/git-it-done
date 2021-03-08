b64db74c1b8f2c876d5aa8191136bf4b66a3fdb6
sshahram <shirin.shahram23@gmail.com>
2021-03-07 18:05:52 -0800

    handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,14 +3,24 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repos was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
 };
 
 var getRepoIssues = function(repo) {
     var apiUrl ="https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if(response.ok) {
@@ -24,7 +34,8 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!")
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
